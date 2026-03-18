a78dbbd83374a3ce19442e6f20a611762cb10e2f
Ashutosh Ardak <ardakashutosh05@gmail.com>
2026-03-18 08:01:22 +0000

    add

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route(/)
+@app.route("/")
 def home():
-    return Hello Jannu DevOps!
+    return "Hello Jannu DevOps!"
 
-if __name__ == __main__:
-    app.run(host=0.0.0.0, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
